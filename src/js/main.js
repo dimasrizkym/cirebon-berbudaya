9922a939317c600502ce4b648177e98924f43f86
korban-mantan <mubarokakbar13@gmail.com>
2023-12-16 04:51:21 +0700

    TODO: FINISHING

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
 new Swiper(".swiperEvent", {
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   spaceBetween: 32,
 });
+
+const learnMore = document.getElementById("learn-more");
+const sectionBudaya = document.getElementById("budaya-cirebon");
+
+learnMore.onclick = ()=>{
+  sectionBudaya.scrollIntoView()
+}
