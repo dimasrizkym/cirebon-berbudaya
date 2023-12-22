606996b5e33df2ef0cc0003cec573900bdf53aa2
korban-mantan <mubarokakbar13@gmail.com>
2023-12-22 13:20:22 +0700

    cleaning up styling and css code

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
 new Swiper(".swiperEvent", {
-  slidesPerView: "auto",
-  spaceBetween: 32,
-});
+    slidesPerView: "auto",
+    spaceBetween: 16
+}
+);
 
 const learnMore = document.getElementById("learn-more");
 const sectionBudaya = document.getElementById("budaya-cirebon");
 
-learnMore.onclick = ()=>{
+learnMore.onclick = () => {
   sectionBudaya.scrollIntoView()
 }
